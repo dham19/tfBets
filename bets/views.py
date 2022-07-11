b6b65f4b1981256530e7190e87bb1775fbfdead0
dham19 <david.ham0099@gmail.com>
2022-07-11 11:30:34 -0400

    basic bets page that does nothng

--- bets/views.py
+++ bets/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    
+    template = loader.get_template('bets/index.html')
+    context = { "items" : [3, 2, 1, 4, 5]
+    }
+    return HttpResponse(template.render(context, request))
